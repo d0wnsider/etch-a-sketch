81bb786ca482cdb30b59b185b05f2dba296f8e6e
d0wnsider <jmhandsamson@gmail.com>
2023-02-24 15:28:40 -0500

    Added a hover function to color the elements

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,27 @@
-let row = 16;
-let column = 16;
-const container = document.querySelector('.container');
-const squareSelect = document.querySelectorAll('div');
-
-
 function gridCalculate () {
     for (let x = 0; x < row; x++) {
         for (let y = 0; y < column; y++) {
             const square = document.createElement('div');
-            square.className = 'color';
+            square.className = 'square';
             square.textContent = y + 1;
             container.appendChild(square);
         }
     }
 }
 
+function squareColorHover () {
+    // hovering color using forEach on Nodelist squareColor and putting addEventListener each element
+    const squareColor = document.querySelectorAll('.square');
+    squareColor.forEach(item => {
+        item.addEventListener('mouseenter',() => {
+            item.style.backgroundColor = 'orange';
+        });
+    });
+}
+
+let row = 16;
+let column = 16;
+const container = document.querySelector('.container');
 
 gridCalculate();
-
-squareSelect.addEventListener('mouseover', () => {
-    squareSelect[0].style.backgroundColor = 'orange';
-})
+squareColorHover();
